8ee97ca0b225776dd257f264c90e6fb2ffe53a23
Sebastian Safari <ssebs@ymail.com>
2016-08-24 16:25:11 -0700

    Implemented home page generation.
    Need to add rules to parse the text. Asof now, valid html will parse
    perfectly

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,93 @@
-// (Simple) Site Generator
-// @author: Sebastian Safari
+import java.util.ArrayList;
 
-public class Main 
+/**(Simple) Site Generator
+* @author Sebastian Safari (ssebs)
+* Idea for the program: generate website from txt files
+* 	TODO:-Bootstrap Styling
+* 	TODO:-Different layouts of pages: Home, Text Only, "Portfolio", Gallery, Contact
+*/
+
+public class Main
 {
-    public static void main(String[] args)
-    {
-        //This will run an instance of the site Generator.
-        System.out.println("Welcome to the Site Generator");
-        
-        
-        
-        
-        //Util.writeFile("Some file stuff\r\nNew line too.", "txt/textFile.txt");
-        //System.out.println(Util.readFile("txt/textFile.txt"));
-    }
-    
-    
-} // End Main class
+
+	private static final String _HEAD_PATH = "genFiles/head.html";
+	private static final String _BODY_CONTENT_PATH = "genFiles/bodyContents.html";
+	private static final String _BODY_END_PATH = "genFiles/bodyEnd.html";
+	private static final String _HOME_PAGE_PATH = "__site/index.html";
+
+	private static String _author, _description;
+
+	private static void init()
+	{
+		ArrayList<String> vars = new ArrayList<String>();
+		vars = Util.readFileToArrayList("genFiles/var.txt");
+		for (String s : vars)
+		{
+			if (s.startsWith("author:"))
+			{
+				_author = s.substring("author:".length());
+			} else if (s.startsWith("description:"))
+			{
+				_description = s.substring("description:".length());
+			}
+		}
+	}
+
+	private static void replaceHome()
+	{
+		// Syntax of these files are subject to change.
+		ArrayList<String> homeLines = new ArrayList<String>();
+
+		homeLines = Util.readFileToArrayList("_textSite/Home.txt");
+
+		// Below generates the metadata in the head.
+		for (String s : homeLines)
+		{
+			if (s.startsWith("MetaTitle:"))
+			{
+				Util.replaceStringInFIle(_HEAD_PATH, "#TITLE#", s.substring("MetaTitle:".length()));
+			}
+		}
+		Util.replaceStringInFIle(_HEAD_PATH, "#AUTHOR#", _author);
+		Util.replaceStringInFIle(_HEAD_PATH, "#DESCRIPTION#", _description);
+
+		// Below generates the body contents
+
+		Util.writeFile("<!-- This content is generated -->", _BODY_CONTENT_PATH);
+		for (String s : homeLines)
+		{
+			if (s.startsWith("<Home>") || (s.startsWith("MetaTitle:")) || (s.startsWith("BodyTitle:")))
+			{
+				/*Having the code like this makes it easier to read. Above is selecting 
+				  	the stuff that's not the body contents.*/
+			} else
+			{
+				//TODO: Add rules to this
+				Util.appendFile(s + System.lineSeparator(), _BODY_CONTENT_PATH); // Will add every line, needs rules
+			}
+		}
+		Util.appendFile("<!-- This content is generated -->", _BODY_CONTENT_PATH);
+
+	}
+
+	private static void generateHome()
+	{
+		String head = Util.readFileToString(_HEAD_PATH);
+		String body = Util.readFileToString(_BODY_CONTENT_PATH);
+		String bodyEnd = Util.readFileToString(_BODY_END_PATH);
+
+		Util.writeFile(head + body + bodyEnd, _HOME_PAGE_PATH);
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome to the Site Generator.");
+		init();
+		replaceHome();
+		generateHome();
+		System.out.println("index page has been generated at: " + _HOME_PAGE_PATH);
+
+		System.out.println("Completed. Please see \"__site\" to view generated site files.");
+	}
+
+} // End Main class 
--- src/Util.java
+++ src/Util.java
@@ -8,33 +8,60 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
-public class Util {
-	public static String readFile(String path) {
+public class Util
+{
+	public static String readFileToString(String path)
+	{
 
-		try {
+		try
+		{
 			BufferedReader br = new BufferedReader(new FileReader(path));
 
 			StringBuilder sb = new StringBuilder();
 			String line = br.readLine();
 
-			while (line != null) {
+			while (line != null)
+			{
 				sb.append(line);
-				sb.append("\n");
+				sb.append("\r\n");
 				line = br.readLine();
 			}
 			br.close();
 			return sb.toString();
-		} catch (Exception e) {
+		} catch (Exception e)
+		{
 			e.printStackTrace();
 			return "String Failed to Load.";
-
 		}
-
 	}
 
-	public static boolean writeFile(String content, String path) {
-		try {
+	public static ArrayList<String> readFileToArrayList(String path)
+	{
+		try
+		{
+			ArrayList<String> ret = new ArrayList<String>();
+			BufferedReader br = new BufferedReader(new FileReader(path));
+			String line = br.readLine();
+			while (line != null)
+			{
+				ret.add(line);
+				line = br.readLine();
+			}
+			br.close();
+			return ret;
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public static boolean writeFile(String content, String path)
+	{
+		try
+		{
 			File file = new File(path);
 			file.getParentFile().mkdirs();
 
@@ -44,11 +71,42 @@ public class Util {
 			wr.close();
 
 			return true;
-		} catch (IOException e) {
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	public static boolean appendFile(String content, String path)
+	{
+		try
+		{
+			File file = new File(path);
+			file.getParentFile().mkdirs();
+
+			FileWriter wr = new FileWriter(file, true);
+
+			wr.write(content);
+			wr.close();
+
+			return true;
+		} catch (IOException e)
+		{
 			e.printStackTrace();
 		}
 
 		return false;
+	}
+
+	public static boolean replaceStringInFIle(String path, String from, String to)
+	{
+		String out = "";
+		String ret = "";
+		out = readFileToString(path);
+		ret = out.replaceAll(from, to);
+
+		return writeFile(ret, path);
 	}
 
 } //End Util Class
